accd9d07f46c7073ecd03b31dc4a62c0963a6d37
mansiarora <mansi@intelligrape.com>
2015-08-17 12:57:16 +0530

    Added bootstrap data for User domain;

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,12 @@
+import image.sharedit.User
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (!User.count) {
+            new User(firstName: 'Mansi', lastName: 'Arora', email: 'mansi.arora@tothenew.com', password: 'password').save(flush: true, failOnError: true)
+            new User(firstName: 'Neha', lastName: 'Gupta', email: 'neha.gupta@tothenew.com', password: 'password').save(flush: true, failOnError: true)
+        }
     }
     def destroy = {
     }
