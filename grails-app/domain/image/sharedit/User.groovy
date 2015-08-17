04fbd70a35d06185cf070021a580dfbae1e9f7a0
mansiarora <mansi@intelligrape.com>
2015-08-17 12:56:21 +0530

    Added logic to create random identificationKey for the user, add this value to USER object in beforeInsert block;

--- grails-app/domain/image/sharedit/User.groovy
+++ grails-app/domain/image/sharedit/User.groovy
@@ -5,6 +5,7 @@ class User {
     String lastName
     String password
     String email
+    String identificationKey = 'randomString'
 
     static hasMany = [images : Image]
 
@@ -13,6 +14,17 @@ class User {
         lastName(nullable: false, blank: false)
         email blank: false, unique: true, email: true, nullable: false
         password blank: false, nullable: false
+        identificationKey unique: false
+    }
+
+    def beforeInsert () {
+        putIdentificationKey()
+    }
+
+    private void putIdentificationKey() {
+        UUID uuid = UUID.randomUUID()
+        char[] uuidChars = uuid.toString().toCharArray()
+        identificationKey = (uuidChars[0]..uuidChars[19]).join('')
     }
 
     @Override
