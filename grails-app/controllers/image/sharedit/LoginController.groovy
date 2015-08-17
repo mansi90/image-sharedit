fc0c420192b6b45038e0f9756d5a1ca9617ced9d
mansiarora <mansi@intelligrape.com>
2015-08-17 17:51:23 +0530

    Added logic to handle valid and invalid login;

--- grails-app/controllers/image/sharedit/LoginController.groovy
+++ grails-app/controllers/image/sharedit/LoginController.groovy
@@ -2,5 +2,16 @@ package image.sharedit
 
 class LoginController {
 
-    def index() { }
+    def index() {}
+
+    def onSubmit() {
+        User user = User.findByEmailAndPassword(params.email, params.password)
+        if (user) {
+            session.userIdentificationKey = user.identificationKey
+            redirect(controller: 'image', action: 'index')
+        } else {
+            flash.message = "Invalid email password combination!"
+            redirect(action: 'index')
+        }
+    }
 }
