bcd14cf355a2188cfb645021a16a11d9c9f9b1dd
mansiarora <mansi@intelligrape.com>
2015-09-08 12:34:11 +0530

    Added Login & Logout Controller;

---
+++ grails-app/controllers/image/sharedit/LogoutController.groovy
@@ -0,0 +1,13 @@
+package image.sharedit
+
+import grails.plugin.springsecurity.SpringSecurityUtils
+
+class LogoutController {
+
+	/**
+	 * Index action. Redirects to the Spring security logout uri.
+	 */
+	def index = {
+        redirect uri: SpringSecurityUtils.securityConfig.logout.filterProcessesUrl // '/j_spring_security_logout'
+	}
+}
