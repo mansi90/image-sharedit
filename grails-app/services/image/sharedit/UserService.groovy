b1f6b0943430cdb7029d119753d7b6f1495c00d4
mansiarora <mansi@intelligrape.com>
2015-09-09 17:20:49 +0530

    Role Management : Added logic in UserService to add/remove/update roles of a user;

--- grails-app/services/image/sharedit/UserService.groovy
+++ grails-app/services/image/sharedit/UserService.groovy
@@ -1,9 +1,43 @@
 package image.sharedit
 
 import grails.transaction.Transactional
+import org.springframework.dao.DataIntegrityViolationException
 
 @Transactional
 class UserService {
+    def messageSource
 
-    def someMethod(){}
+    void saveUserAndItsRole(User userInstance, def roles) {
+        try {
+            userInstance.save(flush: true)
+            Set<Role> oldRoles = userInstance?.authorities
+            if (roles?.class == java.lang.String) {
+                oldRoles ? removeRoles(userInstance, oldRoles) : ''
+                new UserRole(user: userInstance, role: Role.findByAuthority(roles)).save(failOnError: true, flush: true)
+            } else {
+                Set<Role> newRoles = Role.findAll { roles.contains(it.authority) }.toSet()
+                if ((newRoles != oldRoles) && newRoles && oldRoles) {
+                    removeRoles(userInstance, oldRoles)
+                    addRoles(userInstance, newRoles)
+                } else if ((!oldRoles) && newRoles) {
+                    addRoles(userInstance, newRoles)
+                }
+            }
+        } catch (e) {
+            e.printStackTrace(System.out)
+        }
+    }
+
+    def addRoles(User user, Set<Role> roles) {
+        roles?.each {
+            new UserRole(user: user, role: it).save(failOnError: true, flush: true)
+        }
+    }
+
+    def removeRoles(User user, Set<Role> roles) {
+        List<UserRole> userRoleToBeRemoved = UserRole.findAllByRoleInListAndUser(roles as List, user)
+        userRoleToBeRemoved.each { UserRole userRole ->
+            userRole.delete(flush: true)
+        }
+    }
 }
